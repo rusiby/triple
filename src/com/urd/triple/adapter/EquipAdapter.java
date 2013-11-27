a27b4d0f60cbeac03078dd8ecf261dd03780e408
zhangcong <zhangzong@xunlei.com>
2013-11-27 20:15:47 +0800

    解决某些手机装备区显示不出来的bug

--- src/com/urd/triple/adapter/EquipAdapter.java
+++ src/com/urd/triple/adapter/EquipAdapter.java
@@ -2,6 +2,9 @@ package com.urd.triple.adapter;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +15,13 @@ import com.urd.triple.R;
 import com.urd.triple.core.Card;
 
 public final class EquipAdapter extends BaseAdapter {
+    private static final Logger LOG = LoggerFactory.getLogger(EquipAdapter.class);
 
     private List<Card> mEquips;
 
     public void setEquips(List<Card> equips) {
         mEquips = equips;
+        notifyDataSetChanged();
     }
 
     @Override
@@ -36,6 +41,8 @@ public final class EquipAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        LOG.debug("get view p={}", position);
+
         ViewHolder holder = null;
         if (convertView == null) {
             holder = new ViewHolder();
--- src/com/urd/triple/widget/SelfWidget.java
+++ src/com/urd/triple/widget/SelfWidget.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.Gallery;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -130,16 +131,9 @@ public class SelfWidget extends RelativeLayout {
             }
         }
 
-        if (handCards != null) {
-            updateHandCardArea(handCards);
-        }
-
-        if (equipCards != null) {
-            updateEquipArea(equipCards);
-        }
-        if (judgeCards != null) {
-            updateJudgeCardArea(judgeCards);
-        }
+        updateHandCardArea(handCards);
+        updateEquipArea(equipCards);
+        updateJudgeCardArea(judgeCards);
     }
 
     public void updateJudgeCardArea(List<Card> judgeCards) {
